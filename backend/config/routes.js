cdcb55024c1ace959aaf1f8f7226d4c7887cc9fd
willrockies <wlsn.ads@gmail.com>
2019-12-08 23:46:16 -0300

    sumario e ajuste na API

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
-module.exports = function(server) {
+module.exports = function (server) {
   const router = express.Router();
   server.use("/api", router);
   //rotas da API
   const billingCycleService = require("../api/billingCycle/billingCycleService");
   billingCycleService.register(router, "/billingCycles");
+
+  const billingSummaryService = require("../api/billingSummary/billingSummaryService");
+  router.route("/billingSummary").get(billingSummaryService.getSummary);
 };
