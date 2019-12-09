f7ee9ad2b7e0dd779557edc90987dda17e32e11f
W. Alves da Silva <w.alves.da.silva@avanade.com>
2019-12-08 21:58:21 -0300

    add Ciclo de pagamento API

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,2 +1,10 @@
 const mongoose = require("mongoose");
 module.exports = mongoose.connect("mongodb://localhost/db_finance");
+
+mongoose.Error.messages.general.required = "O Atributo '{PATH}' é obrigatório.";
+mongoose.Error.messages.Number.min =
+  "O '{VALUE}' informado é menor que o minimo de '{MIN}'.";
+mongoose.Error.messages.Number.max =
+  "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'.";
+mongoose.Error.messages.String.enum =
+  "'{VALUE}' não é valido para o atributo '{PATH}'.";
